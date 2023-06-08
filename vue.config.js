ec41ddec95afc99c62c74baf486d4192d52aebbd
solitaryorz <solitaryorz@outlook.com>
2023-06-08 22:58:03 +0800

    feat: 修改webpack配置不检测Host

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   devServer: {
+    allowedHosts: "all",
     proxy: {
       '/api': {
         target: 'https://api.bgm.tv',
